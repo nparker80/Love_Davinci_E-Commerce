1ed105f747d7f8bea4d31ecea67093ef26b064b8
Najla Parker <najla08@gmail.com>
2022-08-19 08:30:42 -0600

    Adding REDUX actions, consts & reducers

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,16 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { combineReducers, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
-// import { composeWithDevTools } from "redux-devtools-extension";
-import { cartReducer } from "./reducers/cartReducers";
+import cartReducer from "./reducers/cartReducers";
 
-// const reducer = combineReducers({
-//   cart: cartReducer,
-// })
-
-// const middleware = [thunk];
-
-const store = configureStore({ reducer: { cartReducer, } })
-// , composeWithDevTools(applyMiddleware(...middleware)));
-
-export default store;
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer
+  }
+})
